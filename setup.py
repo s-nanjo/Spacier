3ddec616279a5c126d0471fb66aeb2f06efc1711
ari14663 <fin_ari@jsr.co.jp>
2024-03-18 21:06:31 +0900

    fix some part in setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,25 @@ from setuptools import setup, find_packages
 setup(
     name='spacier',
     version='0.3.0',
-    author='Your Name',
-    author_email='youremail@example.com',
+    author='Shun Nanjo, Arifin',
+    author_email='nanjos@ism.ac.jp, arifin@ism.ac.jp',
     description='A library for efficient exploration of materials space,'
                 'focusing on polymers.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/spacier',
+    url='https://github.com/s-nanjo/Spacier',
     packages=find_packages(),
     install_requires=[
-        'numpy>=1.18.5',
-        'pandas>=1.0.5',
-        'scipy>=1.5.0',
-        'scikit-learn>=0.23.1',
+        'numpy',
+        'pandas',
+        'scipy',
+        'scikit-learn',
     ],
     extras_require={
         'advanced': [
-            'GPy>=1.9.9',
-            'torch>=1.5.0',
-            'gpytorch>=1.2.0'
+            'GPy',
+            'torch',
+            'gpytorch',
         ]
     },
     classifiers=[
